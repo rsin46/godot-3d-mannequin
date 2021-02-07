c8aebe5b4de99f2524a5e78f7aa10f89275dbbff
Rohan Singh <rohan@headspin.io>
2021-02-07 21:39:15 +1100

    Added redrawing ground

--- godot/src/Main/Game.gd
+++ godot/src/Main/Game.gd
@@ -1,9 +1,10 @@
 extends Node
 
 
+
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
+	$TestMap.player = $Player
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("click"):
